f0d4b55e8ed9ba443a476bf86dd70441f62304b6
Simran Simran <736238@student.tor.loc>
2019-05-16 17:42:48 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>simran page</title>
+        <body>
+            <script>
+let item1price = 20;
+let item2price = 30;
+let firstname = "sim";
+let lastname = "verma";
+alert("hello"  + firstname + " "  +lastname+ "your total is"+item1price +item2price);
+           </script>
+        </body>
+    </head>
